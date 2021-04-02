8ba2312247388546cca704278ca531887ec174e0
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-04-02 14:54:48 +0100

    Added new curses

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 var curses = [
-    "You become a duck",
-    "What is this",
-    "Woah another curse?",
-    "I just need random text for tests",
-    "Curses shall come later"
+    "You become a duck for an hour everytime you sneeze",
+    "Blinking has a 1/6th chance of teleporting you to a random location",
+    "You become overwhelmed with homicidal tendencies whenever in close proximity with geese",
+    "You now have a pair of gills, and can now only breathe in water",
+    "You're an arsonist now",
+    "Instead of food, you feed off of making people cry",
+    "The only manner of fighting you can participate in is a dance battle",
+    "You constantly need to pee every 10 minutes",
 ]
 
 function newCurse() {
